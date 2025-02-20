b9e98b88612bb08a7df5adb3b1c2d11b276f2be9
adity72dd <mmunnabot7@gmail.com>
2025-02-20 11:36:05 +0530

    Update t.py

--- t.py
+++ t.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 )
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '7246047709:AAElzJRbgodpAq62ql3aSF2CVXkMbHqdzvA'  # Replace with your bot token
+TELEGRAM_BOT_TOKEN = '7623380258:AAHtmKVKzNvumZyU0-GdOZ2WJ3a5XJSeMxw'  # Replace with your bot token
 OWNER_USERNAME = "Riyahacksyt"  # Replace with your Telegram username (without @)
 ALLOWED_GROUP_ID = -1002283210199  # Replace with your allowed group ID
 MAX_THREADS = 4000  # Default maximum number of threads allowed per attack
